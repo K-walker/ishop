d6e33cd7a9e270b2b5f647df27e11548b539afab
king <huwk@billbear.cn>
2020-03-20 15:50:50 +0800

    测试

--- hook.go
+++
@@ -1,7 +0,0 @@
-public function exec_shell() {
-  set_time_limit(3 * 60); //最大过期时间3分钟
-  $shellPath = "/home/www/ishop";
-  $cmd = "cd $shellPath && sudo git pull && sh CI.sh";
-  $res = $this -> doShell($cmd);
-  print_r($res); // 主要打印结果给github记录查看，自己测试时查看
-}
